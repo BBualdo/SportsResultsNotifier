e4080cc6a48df3cb23c18e0e10b5947dd62491dc
Sebastian <mrseb10@gmail.com>
2024-04-22 12:39:13 +0200

    Model classes

---
+++ SportsResultsNotifier.BBualdo/Models/Match.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsResultsNotifier.BBualdo.Models;
+
+internal class Match
+{
+  public Team Winner { get; set; }
+  public Team Loser { get; set; }
+
+  public Match(Team winner, Team loser)
+  {
+    Winner = winner;
+    Loser = loser;
+  }
+}
